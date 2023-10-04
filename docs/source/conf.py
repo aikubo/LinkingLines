97c505a8d2c8e672ffa598a1a8b8f84441a73981
Allison Kubo Hutchison <alli.i.kubo@gmail.com>
2023-10-04 12:23:30 -0700

    trying to get index.rst to work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,15 +1,15 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./'))
-sys.path.insert(0, os.path.abspath('../linkinglines'))
-sys.path.insert(0, os.path.abspath('../linkinglines/HT'))
-sys.path.insert(0, os.path.abspath('../linkinglines/ProcessingUtils'))
-sys.path.insert(0, os.path.abspath('../linkinglines/FeatureExtraction'))
-sys.path.insert(0, os.path.abspath('../linkinglines/PlotUtils'))
-sys.path.insert(0, os.path.abspath('../linkinglines/ClusterLines'))
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../linkinglines'))
+sys.path.insert(0, os.path.abspath('../../linkinglines/HT'))
+sys.path.insert(0, os.path.abspath('../../linkinglines/ProcessingUtils'))
+sys.path.insert(0, os.path.abspath('../../linkinglines/FeatureExtraction'))
+sys.path.insert(0, os.path.abspath('../../linkinglines/PlotUtils'))
+sys.path.insert(0, os.path.abspath('../../linkinglines/ClusterLines'))
 
 
-
+#import linkinglines
 
 
 # Configuration file for the Sphinx documentation builder.
@@ -40,6 +40,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+# source_suffix = ['.rst', '.md']
+source_suffix = ".rst"
+
+# The encoding of source files.
+# source_encoding = 'utf-8-sig'
+
+# The master toctree document.
+master_doc = "index"
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -49,4 +60,6 @@ html_static_path = ['_static']
 html_theme_options = {
     'github_user': 'aikubo',
     'github_repo': 'Linking-and-Clustering-Dikes',
-    'codecov_button': 'true'}
+    "github_banner": True,
+    "show_related": False,
+    "note_bg": "#FFF59C"}
